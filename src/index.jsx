642660e7acfcf5a58ac27a77b2feca21ccb8b739
Danil P <danil.polovinkin.1994@gmail.com>
2023-05-15 13:32:34 +0300

    init

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = document.querySelector("#root");
+
+ReactDOM.createRoot(root).render(
+  <div>
+    <p>Hello</p>
+  </div>
+);
