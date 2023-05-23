d10d34d17a04e15dfa5ec845f8fb62530f9bf57f
Danil P <danil.polovinkin.1994@gmail.com>
2023-05-23 12:51:53 +0300

    completed tablet ver terms page

---
+++ src/components/molecules/dropText/DropText.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import s from "./dropText.module.scss";
+
+const DropText = ({ arr }) => {
+  const Res = arr.map((el) => {
+    const [state, setState] = useState(false);
+
+    return (
+      <div className={`${s.drop} ${state ? s.active : "none"}`} onClick={() => setState(!state)}>
+        <div className={s.dropTop}>
+          <p className={s.dropTopTitle}>{el.title}</p>
+          <img
+            className={s.dropImg}
+            src={require("../../../assets/img/components/Terms/Arrow-Bottom.svg")}
+          />
+        </div>
+        <div className={s.dropText}>
+          Vehicle Approval Centre may, in its sole discretion and for any reason, modify, supplement
+          or amend these Terms and Conditions without any notice or liability to you or any other
+          person, by posting revised Terms and Conditions on the Vehicle Approval Centre website.
+          Your continued use of the Vehicle Approval Centre website signifies your acceptance of
+          such revised Terms and Conditions.
+        </div>
+      </div>
+    );
+  });
+
+  return Res;
+};
+
+export default DropText;
--- src/components/templates/TermsPage/TermsPage.jsx
+++ src/components/templates/TermsPage/TermsPage.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 import PageTitle from "../../molecules/pageTitle/PageTitle";
+import DropText from "../../molecules/dropText/DropText";
+import s from "./terms.module.scss";
 
 const TermsPage = () => {
   return (
     <div>
       <PageTitle text={"Terms & Conditions"} />
+      <div className={`container ${s.termsWr}`}>
+        <DropText
+          arr={[
+            {
+              title:
+                "Please read these terms and conditions of use carefully before using this web site",
+            },
+            { title: "Modifications of the terms and conditions" },
+            { title: "How we work" },
+            { title: "Authorization" },
+            { title: "Links" },
+            { title: "Minors" },
+            { title: "Restricted access" },
+            { title: "Governing law" },
+            { title: "Privacy policy" },
+            { title: "Personal information" },
+            { title: "Collecting personal information" },
+            { title: "Using and sharing personal information" },
+            { title: "Sharing your personal information" },
+            { title: "Retaining your personal information" },
+            { title: "Contacting you" },
+            { title: "Accessing your personal information stored by us" },
+            { title: "Responding to privacy concerns or complaints" },
+          ]}
+        />
+      </div>
     </div>
   );
 };
