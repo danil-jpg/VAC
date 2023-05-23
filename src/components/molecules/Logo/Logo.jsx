f89aa0e97da26b05ca9fee061d87057f9629ff88
Danil P <danil.polovinkin.1994@gmail.com>
2023-05-23 13:01:15 +0300

    added link to main Page

--- src/components/molecules/Logo/Logo.jsx
+++ src/components/molecules/Logo/Logo.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import s from "./logo.module.scss";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Logo = () => {
+  const navigation = useNavigate();
+
   return (
-    <div className={s.logoWr}>
+    <div className={s.logoWr} onClick={() => navigation("/")}>
       <img src={require("../../../assets/img/components/logo/logo img.png")}></img>
       <p>VAC</p>
     </div>
