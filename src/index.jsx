7156ff03ea9a1ef2c621a21bd52b169941fe0b9a
Danil P <danil.polovinkin.1994@gmail.com>
2023-05-29 11:34:10 +0300

    test

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-const root = document.querySelector("#root");
-
+const root = document.getElementById("root");
+console.log(root);
 ReactDOM.createRoot(root).render(<App />);
