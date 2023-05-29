187e85224bde9a617a654140aba03214532137bb
Danil P <danil.polovinkin.1994@gmail.com>
2023-05-29 10:38:18 +0300

    compiled to build

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import styles from "./assets/styles/styles.scss";
 import Header from "./components/organisms/header/Header.jsx";
 import MainPage from "./components/templates/mainPage/MainPage";
 import Footer from "./components/organisms/footer/footer";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
 import TermsPage from "./components/templates/TermsPage/TermsPage";
 import PolicyPage from "./components/templates/policyPage/PolicyPage";
 import ModalMenu from "./components/organisms/modalMenu/ModalMenu";
@@ -22,7 +22,7 @@ const App = () => {
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Header setMenu={setMenu} setIsQuiz={setIsQuiz} />
         <Routes>
           <Route index element={<MainPage index />}></Route>
@@ -38,7 +38,7 @@ const App = () => {
         </Routes>
         <Footer quiz={isQuiz} />
         <ModalMenu menu={menu} setMenu={setMenu} setIsQuiz={setIsQuiz} />
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 };
