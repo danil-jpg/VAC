b6d798cd65502155fcdbc3423b7c4c65f21dd1d6
Danil P <danil.polovinkin.1994@gmail.com>
2023-05-29 12:20:11 +0300

    opt teset

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,9 +1,8 @@
 const Webpack = require("webpack");
 const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 const path = require("path")
-const TerserPlugin = require("terser-webpack-plugin")
-// const BundleAnalyzer = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+const CssMin = require("css-minimizer-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -26,51 +25,64 @@ module.exports = {
         type: "asset",
         generator: {
           filename: 'images/[hash][ext][query]'
-        }
+        },
+        use: [
+          {
+            loader: `img-optimize-loader`,
+            options: {
+              compress: {
+                webp: true,
+                disableOnDevelopment: true,
+              },
+            },
+          }
+        ]
       },
     ]
   },
-  // optimization: {
-  //   minimizer: [
-  //     "...",
-  //     new ImageMinimizerPlugin({
-  //       minimizer: {
-  //         implementation: ImageMinimizerPlugin.imageminMinify,
-  //         options: {
-  //           // Lossless optimization with custom option
-  //           // Feel free to experiment with options for better result for you
-  //           plugins: [
-  //             ["gifsicle", { interlaced: true }],
-  //             ["jpegtran", { progressive: true }],
-  //             ["optipng", { optimizationLevel: 5 }],
-  //             // Svgo configuration here https://github.com/svg/svgo#configuration
-  //             [
-  //               "svgo",
-  //               {
-  //                 plugins: [
-  //                   {
-  //                     name: "preset-default",
-  //                     params: {
-  //                       overrides: {
-  //                         removeViewBox: false,
-  //                         addAttributesToSVGElement: {
-  //                           params: {
-  //                             attributes: [
-  //                               { xmlns: "http://www.w3.org/2000/svg" },
-  //                             ],
-  //                           },
-  //                         },
-  //                       },
-  //                     },
-  //                   },
-  //                 ],
-  //               },
-  //             ],
-  //           ],
-  //         },
-  //       },
-  //     }),
-  //     new TerserPlugin()
-  //   ],
-  // },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMin(),
+      new HtmlMinimizerPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            // Lossless optimization with custom option
+            // Feel free to experiment with options for better result for you
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              // Svgo configuration here https://github.com/svg/svgo#configuration
+              [
+                "svgo",
+                {
+                  plugins: [
+                    {
+                      name: "preset-default",
+                      params: {
+                        overrides: {
+                          removeViewBox: false,
+                          addAttributesToSVGElement: {
+                            params: {
+                              attributes: [
+                                { xmlns: "http://www.w3.org/2000/svg" },
+                              ],
+                            },
+                          },
+                        },
+                      },
+                    },
+                  ],
+                },
+              ],
+            ],
+          },
+        },
+      }),
+      // new TerserPlugin()
+    ],
+  },
 };
